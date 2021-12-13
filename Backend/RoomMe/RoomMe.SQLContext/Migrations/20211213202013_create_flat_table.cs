f2359478ffa9f272a11918d52cabe52414302bdd
mateuszbasiak <basiak.mat@gmail.com>
2021-12-13 21:21:58 +0100

    [Room-20] Flat table created

---
+++ Backend/RoomMe/RoomMe.SQLContext/Migrations/20211213202013_create_flat_table.cs
@@ -0,0 +1,148 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RoomMe.SQLContext.Migrations
+{
+    public partial class create_flat_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_users_AuthorId",
+                table: "products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_users",
+                table: "users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_products",
+                table: "products");
+
+            migrationBuilder.RenameTable(
+                name: "users",
+                newName: "Users");
+
+            migrationBuilder.RenameTable(
+                name: "products",
+                newName: "Products");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_products_AuthorId",
+                table: "Products",
+                newName: "IX_Products_AuthorId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Users",
+                table: "Users",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Flats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Flats", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FlatUser",
+                columns: table => new
+                {
+                    FlatsId = table.Column<int>(type: "int", nullable: false),
+                    UsersId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FlatUser", x => new { x.FlatsId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_FlatUser_Flats_FlatsId",
+                        column: x => x.FlatsId,
+                        principalTable: "Flats",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FlatUser_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FlatUser_UsersId",
+                table: "FlatUser",
+                column: "UsersId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Users_AuthorId",
+                table: "Products",
+                column: "AuthorId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Users_AuthorId",
+                table: "Products");
+
+            migrationBuilder.DropTable(
+                name: "FlatUser");
+
+            migrationBuilder.DropTable(
+                name: "Flats");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Users",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "Users",
+                newName: "users");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "products");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_AuthorId",
+                table: "products",
+                newName: "IX_products_AuthorId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_users",
+                table: "users",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_products",
+                table: "products",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_users_AuthorId",
+                table: "products",
+                column: "AuthorId",
+                principalTable: "users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
