c6d3d252ddaba1d5cd5c303607a5b436ffff07f1
mateuszbasiak <basiak.mat@gmail.com>
2021-12-14 17:02:32 +0100

    [Room-25] Base flat endpoints created

---
+++ Backend/RoomMe/RoomMe.API/Models/UserNicknameModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoomMe.API.Models
+{
+    public class UserNicknameModel
+    {
+        public int Id { get; set; }
+        public string Nickname { get; set; }
+    }
+}
