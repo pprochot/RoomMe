22164e4974a8b9bfa0497e54775b6407cb96d38b
pawel <pprochot16@gmail.com>
2022-01-18 18:10:36 +0100

    Update UserInfo fragment

--- Android-app/app/src/main/java/uj/roomme/fragments/UserInfoFragment.kt
+++ Android-app/app/src/main/java/uj/roomme/fragments/UserInfoFragment.kt
@@ -17,8 +17,8 @@ class UserInfoFragment : Fragment(R.layout.fragment_user_info) {
 
         activity?.findViewById<BottomNavigationView>(R.id.bottom_nav_bar)
             ?.visibility = View.GONE
-        view?.findViewById<TextView>(R.id.text_username_input)?.text = userViewModel.userNickname
-        view?.findViewById<TextView>(R.id.text_email_input)?.text = userViewModel.userEmail
+//        view?.findViewById<TextView>(R.id.text_username_input)?.text = userViewModel.userNickname
+//        view?.findViewById<TextView>(R.id.text_email_input)?.text = userViewModel.userEmail
     }
 
     override fun onStop() {
