03427924ea2b38e7d68e4b236afe41f28f27e2de
pawel <pprochot16@gmail.com>
2022-01-18 18:46:00 +0100

    Create three home layouts

--- Android-app/app/src/main/java/uj/roomme/fragments/HomeFragment.kt
+++ Android-app/app/src/main/java/uj/roomme/fragments/HomeFragment.kt
@@ -3,5 +3,6 @@ package uj.roomme.fragments
 import androidx.fragment.app.Fragment
 import uj.roomme.R
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home_selected_apartment) {
+
 }
--- Android-app/app/src/main/java/uj/roomme/fragments/UserInfoFragment.kt
+++ Android-app/app/src/main/java/uj/roomme/fragments/UserInfoFragment.kt
@@ -1,30 +1,11 @@
 package uj.roomme.fragments
 
-import android.view.View
-import androidx.fragment.app.Fragment
-import android.widget.TextView
 import androidx.fragment.app.activityViewModels
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import uj.roomme.R
+import uj.roomme.abstractfragments.NoBottomNavBarFragment
 import uj.roomme.viewmodels.UserViewModel
 
-class UserInfoFragment : Fragment(R.layout.fragment_user_info) {
+class UserInfoFragment : NoBottomNavBarFragment(R.layout.fragment_user_info) {
 
     private val userViewModel: UserViewModel by activityViewModels()
-
-    override fun onStart() {
-        super.onStart()
-
-        activity?.findViewById<BottomNavigationView>(R.id.bottom_nav_bar)
-            ?.visibility = View.GONE
-//        view?.findViewById<TextView>(R.id.text_username_input)?.text = userViewModel.userNickname
-//        view?.findViewById<TextView>(R.id.text_email_input)?.text = userViewModel.userEmail
-    }
-
-    override fun onStop() {
-        super.onStop()
-
-        activity?.findViewById<BottomNavigationView>(R.id.bottom_nav_bar)
-            ?.visibility = View.VISIBLE
-    }
 }
