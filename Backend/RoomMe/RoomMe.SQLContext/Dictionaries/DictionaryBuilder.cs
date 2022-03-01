a5d5769466df793458757e5d43493ded9a08f0d7
mateuszbasiak <basiak.mat@gmail.com>
2022-03-01 16:39:06 +0100

    [Houseworks] Added statuses dictionary

---
+++ Backend/RoomMe/RoomMe.SQLContext/Dictionaries/DictionaryBuilder.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomMe.SQLContext.Dictionaries
+{
+    static class DictionaryBuilder
+    {
+        public static void AddDictionaries(ModelBuilder modelBuilder)
+        {
+            HouseworkStatusesDictionary.AddRecords(modelBuilder);
+        }
+    }
+}
---
+++ Backend/RoomMe/RoomMe.SQLContext/Dictionaries/HouseworkStatusesDictionary.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RoomMe.SQLContext.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoomMe.SQLContext.Dictionaries
+{
+    static class HouseworkStatusesDictionary
+    {
+        public static void AddRecords(ModelBuilder modelbuilder)
+        {
+            modelbuilder.Entity<HouseworkStatus>().HasData(
+                new { Id = 1, Name = "In Progress" },
+                new { Id = 2, Name = "Done" },
+                new { Id = 3, Name = "Expired" },
+                new { Id = 4, Name = "Delayed" }
+                );
+        }
+    }
+}
