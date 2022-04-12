5eead9e063ac696d894d23fd15d5e89cb7e5d930
mateuszbasiak <basiak.mat@gmail.com>
2022-04-12 20:23:33 +0200

    Remove isDivided

---
+++ Backend/RoomMe/RoomMe.SQLContext/Migrations/20220412182047_remove_column_common_costs.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace RoomMe.SQLContext.Migrations
+{
+    public partial class remove_column_common_costs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDivided",
+                table: "CommonCosts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDivided",
+                table: "CommonCosts",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
